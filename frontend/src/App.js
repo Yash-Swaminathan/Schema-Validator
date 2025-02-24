ac2c657f292ab7793dd1c96e7efe4fdf4c8f1ab9
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-02-24 11:18:46 -0700

    Added Initial Frontend with

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from './React';
+
+function App() {
+  return (
+    <div>
+      <h1>Main App</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
