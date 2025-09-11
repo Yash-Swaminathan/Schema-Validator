beaf9983096defc4acb31561e07b0c42c6701e46
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-09-10 23:30:13 -0400

    Delete Render commits

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Startup script for Render deployment
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Start the FastAPI application
-uvicorn backend.main:APP --host 0.0.0.0 --port $PORT 
